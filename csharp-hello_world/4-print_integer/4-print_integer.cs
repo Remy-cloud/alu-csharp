107827985fff2ab0e6a83f404134246bfa64c76b
Remy-cloud <r.intwali@alustudent.com>
2026-01-17 15:33:22 +0200

    integer

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -3,8 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int numbers;
-        string street = "Mission Street";
+       
         Console.WriteLine(number + " " + street);
     }
 }
