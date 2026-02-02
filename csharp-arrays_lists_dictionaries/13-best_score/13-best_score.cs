49a62b648b1380f857b0293a4fe11356f5e4c681
Remy-cloud <r.intwali@alustudent.com>
2026-02-02 21:16:42 +0200

    biggerst int

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestKey = "None";
+        int bestValue = -1;
+
+        foreach (KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value > bestValue)
+            {
+                bestValue = item.Value;
+                bestKey = item.Key;
+            }
+        }
+
+        return bestKey;
+    }
+}
