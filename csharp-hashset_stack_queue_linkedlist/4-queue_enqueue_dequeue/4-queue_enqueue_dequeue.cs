134e806df4273af709ba1728c13ce6e3088c3775
Remy-cloud <r.intwali@alustudent.com>
2026-02-06 19:28:09 +0200

    enqueque

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        // Print the number of items
+        Console.WriteLine("Number of items: " + aQueue.Count);
+
+        // Print the first item or "Queue is empty"
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("First item: " + aQueue.Peek());
+        }
+
+        // Add the new item
+        aQueue.Enqueue(newItem);
+
+        // Check if queue contains search item
+        bool contains = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"" + search + "\": " + contains);
+
+        // If contains, remove all items up to and including search
+        if (contains)
+        {
+            while (aQueue.Dequeue() != search)
+            {
+                // Keep dequeuing until we find and remove search
+            }
+        }
+
+        return aQueue;
+    }
+}
