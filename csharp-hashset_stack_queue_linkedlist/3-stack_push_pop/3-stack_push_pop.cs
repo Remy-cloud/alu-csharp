7d29853ec8ae5b76ee53e4032ef7a6c45f71017e
Remy-cloud <r.intwali@alustudent.com>
2026-02-06 19:21:59 +0200

    stack push

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        // Print the number of items
+        Console.WriteLine("Number of items: " + aStack.Count);
+
+        // Print the top item or "Stack is empty"
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: " + aStack.Peek());
+        }
+
+        // Check if stack contains search item
+        bool contains = aStack.Contains(search);
+        Console.WriteLine("Stack contains " + search + ": " + contains);
+
+        // If contains, remove all items up to and including search
+        if (contains)
+        {
+            while (aStack.Pop() != search)
+            {
+                // Keep popping until we find and remove search
+            }
+        }
+
+        // Add the new item
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
