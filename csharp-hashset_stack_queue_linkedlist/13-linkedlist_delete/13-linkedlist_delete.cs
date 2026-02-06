1f237d53910a4eb87164473c8daba051cb1deab6
Remy-cloud <r.intwali@alustudent.com>
2026-02-06 19:58:46 +0200

    linked delete

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            return;
+        }
+
+        LinkedListNode<int> current = myLList.First;
+
+        for (int i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        myLList.Remove(current);
+    }
+}
