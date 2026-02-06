d6319e5acbf04fefeef62c664e3c2c1697062dac
Remy-cloud <r.intwali@alustudent.com>
2026-02-06 19:44:53 +0200

    liked get node

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+
+        int index = 0;
+
+        foreach (int item in myLList)
+        {
+            if (index == n)
+            {
+                return item;
+            }
+            index++;
+        }
+
+        return 0;
+    }
+}
