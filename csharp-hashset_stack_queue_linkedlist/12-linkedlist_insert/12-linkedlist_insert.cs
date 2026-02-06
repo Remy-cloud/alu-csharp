568c6d3bb0d8981376d6d8aa798f933d4112220f
Remy-cloud <r.intwali@alustudent.com>
2026-02-06 19:55:13 +0200

    lined insert

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0 || n <= myLList.First.Value)
+        {
+            return myLList.AddFirst(n);
+        }
+
+        LinkedListNode<int> current = myLList.First;
+
+        while (current.Next != null && current.Next.Value < n)
+        {
+            current = current.Next;
+        }
+
+        return myLList.AddAfter(current, n);
+    }
+}
