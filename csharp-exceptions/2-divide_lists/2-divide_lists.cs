44a87f12b716087e3d766f2945c5a4731990dec6
Remy-cloud <r.intwali@alustudent.com>
2026-02-12 21:26:53 +0200

    divide

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -28,7 +28,6 @@ class List
             catch (ArgumentOutOfRangeException)
             {
                 Console.WriteLine("Out of range");
-                result.Add(0);
             }
         }
 
