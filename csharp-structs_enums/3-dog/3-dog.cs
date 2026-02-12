07e91699fbac84c81704facfe1a3d61c4eb048f5
Remy-cloud <r.intwali@alustudent.com>
2026-02-12 22:16:37 +0200

    dog

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        string article = (rating == Rating.Excellent) ? "an" : "a";
+        return $"My name is {name} and I am {age}. I am {article} {rating} dog. :)";
+    }
+}
