1d3148129218be205fbace5a16c3f76cfd3add35
Remy-cloud <r.intwali@alustudent.com>
2026-02-12 22:10:11 +0200

    dog

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -1,7 +1,21 @@
-﻿public struct Dog
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
 {
     public string name;
     public float age;
     public string owner;
     public Rating rating;
+
+    public override string ToString()
+    {
+        return $"My name is {name} and I am {age}. I am a {rating} dog. :)";
+    }
 }
