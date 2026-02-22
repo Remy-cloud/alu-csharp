b15737d1a5562182deabd1bffef0632ba6e3df29
Remy-cloud <r.intwali@alustudent.com>
2026-02-22 13:48:10 +0200

    2-enemy

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// The health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with health set to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with a specified health value.
+        /// Throws ArgumentException if value is less than 0.
+        /// </summary>
+        /// <param name="value">The health value for the zombie.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
