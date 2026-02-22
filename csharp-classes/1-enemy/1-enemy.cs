1b9604055d7a2ea9db0c3cfbcdffae6729b85d9b
Remy-cloud <r.intwali@alustudent.com>
2026-02-22 13:35:52 +0200

    enemy

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,21 @@
+﻿namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// The health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the Zombie class with health set to 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
