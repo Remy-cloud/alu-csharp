a8c7596563cf16bc2ababb84a6a99c9320b5bf63
Remy-cloud <r.intwali@alustudent.com>
2026-02-22 13:28:28 +0200

    class creation

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,16 +1,6 @@
 ﻿namespace Enemies
 {
-    /// <summary>
-    /// Represents a Zombie enemy.
-    /// </summary>
     public class Zombie
     {
-        /// <summary>
-        /// Returns the string representation of the Zombie object.
-        /// </summary>
-        public override string ToString()
-        {
-            return "Enemies.Zombie";
-        }
     }
 }
