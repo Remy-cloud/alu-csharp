a04c4328d96f5823cbc1914d130861817ccbb500
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 23:07:29 +0200

    3 tyoe get

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.Reflection;
 
-namespace _0_is
+/// <summary>
+/// Provides reflection methods for objects.
+/// </summary>
+namespace _3_type_get
 {
     /// <summary>
-    /// Provides object reflection methods.
+    /// Contains methods to inspect object types.
     /// </summary>
     public class Obj
     {
         /// <summary>
-        /// Prints the names of the available properties and methods of an object.
+        /// Prints the properties and methods of an object.
         /// </summary>
         /// <param name="myObj">The object to inspect.</param>
         public static void Print(object myObj)
         {
-            TypeInfo typeInfo = myObj.GetType().GetTypeInfo();
+            Type type = myObj.GetType();
 
-            Console.WriteLine("Properties:");
-            foreach (PropertyInfo prop in typeInfo.GetProperties())
+            Console.WriteLine($"{type.Name} Properties:");
+            foreach (PropertyInfo property in type.GetProperties())
             {
-                Console.WriteLine(prop.Name);
+                Console.WriteLine(property.Name);
             }
 
-            Console.WriteLine("Methods:");
-            foreach (MethodInfo method in typeInfo.GetMethods())
+            Console.WriteLine($"{type.Name} Methods:");
+            foreach (MethodInfo method in type.GetMethods())
             {
                 Console.WriteLine(method.Name);
             }
