4c2c0d199417c851d0ad0a7393ccc5f73e8138df
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 23:17:40 +0200

    shape

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Represents a geometric shape.
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Returns the area of the shape. Not implemented in base class.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    /// <exception cref="NotImplementedException">Thrown always in base class.</exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
