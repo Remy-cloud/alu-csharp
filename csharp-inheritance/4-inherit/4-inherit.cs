043c063cde8a6f4e9df9df1a208cf80149317178
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 23:14:44 +0200

    inhert

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Represents an animal.
+/// </summary>
+public class Animal
+{
+}
+
+/// <summary>
+/// Represents a dog, which is an animal.
+/// </summary>
+public class Dog : Animal
+{
+}
