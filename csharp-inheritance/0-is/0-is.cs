30c5d730fc0397b33b39c912e58bda4875e4a02b
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 22:24:02 +0200

    returns True if the object is an int

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿namespace ObjTests
+{
+    /// <summary>
+    /// Provides object type checking operations.
+    /// </summary>
+    public class Obj
+    {
+        /// <summary>
+        /// Returns True if the object is an int, otherwise returns False.
+        /// </summary>
+        /// <param name="obj">The object to check.</param>
+        /// <returns>True if obj is int, otherwise False.</returns>
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj is int;
+        }
+    }
+}
