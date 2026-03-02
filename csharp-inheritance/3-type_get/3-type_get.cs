ba55b9dd1a01220dda3e471e5e376535c5f7b3f7
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 23:02:52 +0200

    3 type get

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace _0_is
+{
+    /// <summary>
+    /// Provides object reflection methods.
+    /// </summary>
+    public class Obj
+    {
+        /// <summary>
+        /// Prints the names of the available properties and methods of an object.
+        /// </summary>
+        /// <param name="myObj">The object to inspect.</param>
+        public static void Print(object myObj)
+        {
+            TypeInfo typeInfo = myObj.GetType().GetTypeInfo();
+
+            Console.WriteLine("Properties:");
+            foreach (PropertyInfo prop in typeInfo.GetProperties())
+            {
+                Console.WriteLine(prop.Name);
+            }
+
+            Console.WriteLine("Methods:");
+            foreach (MethodInfo method in typeInfo.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+}
