1b297852aad16cac8027f2b6ff464e59f2c905b7
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 22:50:40 +0200

    true if the obj is instance or inherited from array

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Provides object type checking operations.
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Returns True if the object is an instance of Array or a subclass, otherwise returns False.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if obj is an instance of Array or a subclass, otherwise False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
