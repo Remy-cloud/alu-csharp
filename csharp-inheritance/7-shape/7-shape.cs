b0a3676d21b75b864f78f092a44e4aec9c0fa9e3
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 23:24:24 +0200

    7-shape

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+/// <summary>
+/// Represents a geometric shape.
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Returns the area of the shape. Not implemented in base class.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    /// <exception cref="NotImplementedException">Thrown always in base class.</exception>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Represents a rectangle, which is a shape.
+/// </summary>
+public class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Gets or sets the width of the rectangle.
+    /// </summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the height of the rectangle.
+    /// </summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+
+    /// <summary>
+    /// Returns the area of the rectangle.
+    /// </summary>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    /// <summary>
+    /// Returns a string representation of the rectangle.
+    /// </summary>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
