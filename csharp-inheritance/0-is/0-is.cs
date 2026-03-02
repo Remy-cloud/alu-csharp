acf9d02d9ab6d9b1f0d3962bfc1f528ed5d35173
Remy-cloud <r.intwali@alustudent.com>
2026-03-02 22:45:40 +0200

    changed the namespace

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,18 +1,15 @@
-﻿namespace _0_is
+﻿/// <summary>
+/// Provides object type checking operations.
+/// </summary>
+public class Obj
 {
     /// <summary>
-    /// Provides object type checking operations.
+    /// Returns True if the object is an int, otherwise returns False.
     /// </summary>
-    public class Obj
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if obj is int, otherwise False.</returns>
+    public static bool IsOfTypeInt(object obj)
     {
-        /// <summary>
-        /// Returns True if the object is an int, otherwise returns False.
-        /// </summary>
-        /// <param name="obj">The object to check.</param>
-        /// <returns>True if obj is int, otherwise False.</returns>
-        public static bool IsOfTypeInt(object obj)
-        {
-            return obj is int;
-        }
+        return obj is int;
     }
 }
