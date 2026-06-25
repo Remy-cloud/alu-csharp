4cf8d15680ab62ed13b8c8dc0ddaf5fbba88fc40
Remy-cloud <r.intwali@alustudent.com>
2026-06-25 19:32:25 +0200

    dequegue

--- csharp-generics/2-dequeue/queue.cs
+++ csharp-generics/2-dequeue/queue.cs
@@ -1,93 +1,121 @@
 using System;
 
 /// <summary>
-/// Represents a generic queue.
+/// A generic queue.
 /// </summary>
-/// <typeparam name="T">The type of elements in the queue.</typeparam>
-class Queue<T>
+/// <typeparam name="T">Element type.</typeparam>
+public class Queue<T>
 {
+    /// <summary>
+    /// A node in the queue.
+    /// </summary>
     public class Node
     {
         /// <summary>
-        /// The value of the node.
+        /// Node value.
         /// </summary>
-        public T value;
+        public T Value { get; set; }
+        
+        /// <summary>
+        /// Next node.
+        /// </summary>
+        public Node Next { get; set; }
 
         /// <summary>
-        /// The next node in the queue.
+        /// Creates a node with a value.
         /// </summary>
-        public Node? next = null;
-
-        /// <summary>
-        /// Constructor for the node.
-        /// </summary>
-        /// <param name="val">The value of the node.</param>
-        public Node(T val)
+        /// <param name="value">Node value.</param>
+        public Node(T value)
         {
-            value = val;
+            Value = value;
+            Next = null;
         }
     }
 
-    private Node? head;
-    private Node? tail;
+    /// <summary>
+    /// Queue head.
+    /// </summary>
+    public Node Head { get; private set; }
+    
+    /// <summary>
+    /// Queue tail.
+    /// </summary>
+    public Node Tail { get; private set; }
+    
+    /// <summary>
+    /// Node count.
+    /// </summary>
     private int count;
 
     /// <summary>
-    /// Checks the type of the queue.
+    /// Initializes the queue.
     /// </summary>
-    /// <returns>The type of T.</returns>
-    public Type CheckType()
+    public Queue()
     {
-        return typeof(T);
+        Head = null;
+        Tail = null;
+        count = 0;
     }
 
     /// <summary>
-    /// Adds a new node to the end of the queue.
+    /// Adds a node to the end.
     /// </summary>
-    /// <param name="value">The value to add.</param>
+    /// <param name="value">Node value.</param>
     public void Enqueue(T value)
     {
         Node newNode = new Node(value);
-
-        if (head == null)
+        if (Tail == null)
         {
-            head = newNode;
-            tail = newNode;
+            Head = newNode;
         }
         else
         {
-            tail.next = newNode;
-            tail = newNode;
+            Tail.Next = newNode;
         }
-
+        Tail = newNode;
         count++;
     }
 
     /// <summary>
-    /// Removes the first node in the queue and returns its value.
+    /// Removes the first node from the queue and returns its value.
     /// </summary>
     /// <returns>The value of the first node.</returns>
     public T Dequeue()
     {
-        if (head == null)
+        if (Head == null)
         {
             Console.WriteLine("Queue is empty");
-            return default(T);
+            return default(T); // Return default value for type T (null for reference types, 0 for value types)
         }
 
-        T value = head.value;
-        head = head.next;
+        T value = Head.Value;
+        Head = Head.Next;
         count--;
+
+        if (Head == null)
+        {
+            Tail = null;
+        }
 
         return value;
     }
 
     /// <summary>
-    /// Returns the number of nodes in the queue.
+    /// Gets the node count.
     /// </summary>
-    /// <returns>The number of nodes.</returns>
+    /// <returns>Node count.</returns>
     public int Count()
     {
         return count;
     }
+
+    /// <summary>
+    /// Gets the element type.
+    /// </summary>
+    /// <returns>Element type.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
 }
